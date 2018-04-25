0b8845ce34209ea7281b2bef389f742334a42405
Mohamed khaled <30750283+MuhamedKhaled@users.noreply.github.com>
2018-04-25 19:47:32 +0200

    Update RetrofitInterface.java

--- PopularMovies/app/src/main/java/com/example/mohamed/popularmovies/Retrofit/RetrofitInterface.java
+++ PopularMovies/app/src/main/java/com/example/mohamed/popularmovies/Retrofit/RetrofitInterface.java
@@ -22,21 +22,21 @@ public interface RetrofitInterface {
     // put API key after equal
     // it should be like "movie/popular?api_key=61487d687a6d7s"
     // in both services
-    @GET("movie/popular?api_key=ac695c241478dc6d192486daec5515b0")
+    @GET("movie/popular?api_key=")
     Call<MoviesPage> getPopularMovies();
 
-    @GET("movie/top_rated?api_key=ac695c241478dc6d192486daec5515b0")
+    @GET("movie/top_rated?api_key=")
     Call<MoviesPage> getTopRatedMovies();
 
-     @GET("movie/{id}?api_key=ac695c241478dc6d192486daec5515b0")
+     @GET("movie/{id}?api_key=")
      Call<Movie> getMovie(@Path("id") long id);
 
-    @GET("movie/{id}/videos?api_key=ac695c241478dc6d192486daec5515b0")
+    @GET("movie/{id}/videos?api_key=")
     Call<MovieVideos> getMovieVideos(@Path("id") long id);
 
-    @GET("movie/{id}/reviews?api_key=ac695c241478dc6d192486daec5515b0")
+    @GET("movie/{id}/reviews?api_key=")
     Call<MovieReviews> getMovieReviews(@Path("id") long id);
 
-    @GET("movie/{id}/credits?api_key=ac695c241478dc6d192486daec5515b0")
+    @GET("movie/{id}/credits?api_key=")
     Call<MovieCredits> getMovieCredits(@Path("id") long id);
 }
